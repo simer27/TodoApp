56a48636fac9e33a48f854c750cd674f1170c30f
Simeao Almeida <simer27@yahoo.com>
2024-10-16 13:30:27 -0300

    CRUD TODO Completo

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async findOneOrFail(id: string) {
+    try {
+      return await this.todoRepository.findOneOrFail({ where: { id } });
+    } catch (error) {
+      throw new HttpException(
+        `O id ${id}, não foi encontrado na base de dados.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOneOrFail(id);
+    this.todoRepository.merge(todo, data);
+
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.findOneOrFail(id);
+    await this.todoRepository.softDelete(id);
+  }
+}
